0f8018624341054d49d3bf508ce43483b6bc0634
jgallud <jgallud@gmail.com>
2024-02-13 13:20:20 +0100

    new version

--- maze.py
+++ maze.py
@@ -1,21 +1,44 @@
 class Game:
-    def __init__(self, maze):
+
+    def __init__(self):
+        self.maze = None
+
+    def new_wall(self):
+        return Wall()
+    
+    def new_door(self):
+        return Door()
+
+    def new_room(self, room_name, description, id):
+        return Room(room_name, description, id)
+
+
+    def createMaze2Hab(self):
+        maze = Maze()
         self.maze = maze
+        room1 = Room(1, "Room 1", "This is the first room")
+        room2 = Room(2, "Room 2", "This is the second room")
+
+        door=Door(room1, room2)
+
+        room1.s_to = door  
+        room2.n_to = door
+
+        self.maze.addRoom(room1)
+        self.maze.addRoom(room2)
 
 class MapElement:
     def __init__(self):
         pass
 
 class Maze(MapElement):
-
     def __init__(self):
         self.rooms = []
 
-    def add_room(self, room):
+    def addRoom(self, room):
         self.rooms.append(room)
 
 class Room(MapElement):
-
     def __init__(self, id, name, description):
         self.id = id
         self.name = name
@@ -34,11 +57,6 @@ class Door(MapElement):
         self.from_room = from_room
         self.to_room = to_room
 
-maze = Maze()
-room1 = Room()
-room2 = Room()
+game = Game()
 
-maze.addRoom(room1)
-maze.addRoom(room2)
 
-door = Door(room1, room2)

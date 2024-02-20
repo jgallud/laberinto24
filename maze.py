d612d9f38ff1052edb2e1bd3c590acdf3c406141
jgallud <jgallud@gmail.com>
2024-02-20 13:17:30 +0100

    unit test

--- maze.py
+++ maze.py
@@ -1,37 +1,81 @@
 class Game:
-
     def __init__(self):
         self.maze = None
 
-    def new_wall(self):
+    def makeWall(self):
         return Wall()
     
-    def new_door(self):
-        return Door()
+    def makeDoor(self,room1, room2):
+        door=Door(room1, room2)
+        return door
 
-    def new_room(self, room_name, description, id):
-        return Room(room_name, description, id)
-
+    def makeRoom(self, id):
+        room=Room(id)
+        room.north=self.makeWall()
+        room.east=self.makeWall()
+        room.south=self.makeWall()
+        room.west=self.makeWall()
+        return room
 
     def createMaze2Hab(self):
         maze = Maze()
         self.maze = maze
-        room1 = Room(1, "Room 1", "This is the first room")
-        room2 = Room(2, "Room 2", "This is the second room")
+        room1 = Room(1)
+        room2 = Room(2)
 
         door=Door(room1, room2)
 
-        room1.s_to = door  
-        room2.n_to = door
+        room1.south = door  
+        room2.north = door
 
         self.maze.addRoom(room1)
         self.maze.addRoom(room2)
 
+    def createMaze2HabFM(self):
+        room1 = self.makeRoom(1)
+        room2 = self.makeRoom(2)
+        door = self.makeDoor(room1, room2)
+        maze = Maze()
+        maze.addRoom(room1)
+        maze.addRoom(room2)
+      
+        room1.south = door 
+        room2.north = door
+
+        return maze
+
+class BombedGame(Game):
+  def makeWall(self):
+    return BombedWall()
+  
 class MapElement:
     def __init__(self):
         pass
 
-class Maze(MapElement):
+class Container(MapElement):
+    # Composite
+    def __init__(self):
+        self._children = []
+
+    def add(self, component):
+        self._children.append(component)
+
+    def remove(self, component):
+        self._children.remove(component)
+
+class Leaf(MapElement):
+    def __init__(self):
+        super().__init__()
+
+class Decorator(Leaf):
+    def __init__(self):
+        super().__init__()
+        self.comp = None
+
+class Bomb(Decorator):
+    pass
+
+class Maze(Container):
     def __init__(self):
         self.rooms = []
 
@@ -39,24 +83,35 @@ class Maze(MapElement):
         self.rooms.append(room)
 
 class Room(MapElement):
-    def __init__(self, id, name, description):
+    def __init__(self, id):
         self.id = id
-        self.name = name
-        self.description = description
-        self.n_to = None
-        self.s_to = None
-        self.e_to = None
-        self.w_to = None
+        self.north = None
+        self.south = None
+        self.east = None
+        self.west = None
 
 class Wall(MapElement):
     def __init__(self):
         pass
 
+class BombedWall(Wall):
+    def __init__(self):
+        super().__init__()
+        self.active = False
+
 class Door(MapElement):
-    def __init__(self, from_room, to_room):
-        self.from_room = from_room
-        self.to_room = to_room
+    def __init__(self, side1, side2):
+        self.side1 = side1
+        self.side2 = side2
 
 game = Game()
+game.createMaze2Hab()
+
+bgame = BombedGame()
+bgame.createMaze2Hab()
+
+game = Game()
+game.createMaze2HabFM()
+
 
 
---
+++ test-lab2HabFM.py
@@ -0,0 +1,40 @@
+import unittest
+from maze import Game, Maze, Room, Door
+
+class TestMazeGame(unittest.TestCase):
+
+    def test_create_maze(self):
+        game = Game()
+        maze = game.createMaze2HabFM()
+        
+        self.assertIsInstance(maze, Maze)
+        
+        rooms = maze.rooms
+        self.assertEqual(len(rooms), 2)
+        
+        room1, room2 = rooms
+        
+        self.assertIsInstance(room1, Room)
+        self.assertIsInstance(room2, Room)
+        
+        # Verificar paredes y puertas
+        self.assertIsNotNone(room1.north) 
+        self.assertIsNotNone(room1.south)
+        self.assertIsNotNone(room1.east)
+        self.assertIsNotNone(room1.west)
+        
+        self.assertIsNotNone(room2.north)
+        self.assertIsNotNone(room2.south)
+        self.assertIsNotNone(room2.east)
+        self.assertIsNotNone(room2.west)  
+        
+        # Verificar que las paredes sean distintas instancias
+        self.assertIsNot(room1.north, room2.north)
+        self.assertIsNot(room1.south, room2.south)
+        
+        #verifica que al sur de room1 hay un objeto de tipo puerta
+        self.assertIsInstance(room1.south, Door)
+
+
+if __name__ == '__main__':
+    unittest.main()
